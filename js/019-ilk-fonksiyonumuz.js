1425cdd8290a7dbb18ba1969086984f0ee8cd92f
Aysel Cavdar <a.misrikhanova13@gmail.com>
2022-06-16 14:20:17 +0300

    first function

---
+++ js/019-ilk-fonksiyonumuz.js
@@ -0,0 +1,24 @@
+// ** Ilk Fonksiyonumuzu Tanimlamak:
+
+function hello (){
+    console.log("merhaba");
+    helloWorld()
+}
+
+function helloWorld(){
+    console.log("merhaba dunya");
+}
+
+// hata alabiliriz...
+// function userCheck () {
+//     if (userName && age >= 18) {
+//         info.innerHTML = "ehliyet alabilirsiniz"
+//     } else if (!userName) {
+//         info.innerHTML = "Kullanici Adiniz Yok"
+//     } else if ( !(age >= 18) ) {
+//         info.innerHTML = "Yas Bilginiz Yok veya 18 Yasindan Kucuksunuz"
+//     }
+// }
+
+
+hello() //calistir
